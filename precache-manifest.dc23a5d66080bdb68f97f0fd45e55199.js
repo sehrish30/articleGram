5292e2cc805caa0708083d384535049a941ffea7
Sahresh Waheed <sehrishwaheed98@gmail.com>
2020-08-27 15:41:00 +0300

    Updates

---
+++ precache-manifest.dc23a5d66080bdb68f97f0fd45e55199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2a95b3b3e1f31229cd09ad9e9937d4",
+    "url": "/articleGram/index.html"
+  },
+  {
+    "revision": "cc183257b9a7afc488aa",
+    "url": "/articleGram/static/js/2.1b233561.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/articleGram/static/js/2.1b233561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e19c4f5c7a5685b8833",
+    "url": "/articleGram/static/js/main.0924d835.chunk.js"
+  },
+  {
+    "revision": "6cbb56155f27a9e829e9",
+    "url": "/articleGram/static/js/runtime-main.d7a67d49.js"
+  }
+]);
